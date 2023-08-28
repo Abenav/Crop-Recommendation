d15929fab702ec6ea7cfea7751ad870ef04d07e2
Abenav <73566954+Abenav@users.noreply.github.com>
2023-08-28 18:43:00 +0530

    Update datatocsv.py

--- datatocsv.py
+++ datatocsv.py
@@ -11,21 +11,21 @@ def signal_handler(signal, frame):
     sys.exit(0)
 
 # Set up serial port and communication parameters
-COM = "COM4"
-BAUD = "9600"
-SerialPort = serial.Serial(COM, BAUD, timeout=1)
+COM = "COM4"  # COM port to which the device is connected, change as needed
+BAUD = "9600"  # Baud rate for serial communication
+SerialPort = serial.Serial(COM, BAUD, timeout=1)  # Open a serial connection
 
 # Set up CSV file and header row
 with open('data.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['Incoming Data'])
+    writer.writerow(['Incoming Data'])  # Write a header row to the CSV file
 
-# Continuously read from serial port and store incoming data in CSV file
+# Continuously read from the serial port and store incoming data in the CSV file
 while True:
-    IncomingData = SerialPort.readline().decode('utf-8').rstrip()
-    if IncomingData:
+    IncomingData = SerialPort.readline().decode('utf-8').rstrip()  # Read and decode incoming data
+    if IncomingData:  # Check if there's valid incoming data
 
         with open('data.csv', mode='a', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow([IncomingData])
-    time.sleep(0.01)
+            writer.writerow([IncomingData])  # Write the incoming data to the CSV file
+    time.sleep(0.01)  # Delay to avoid busy-waiting
